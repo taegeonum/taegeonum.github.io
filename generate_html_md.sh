83ece22c468d1568475d11ff959e44835327f23d
taegeonum <taegeonum@gmail.com>
2023-09-28 10:56:17 +0900

    fix

--- generate_html_md.sh
+++ generate_html_md.sh
@@ -4,10 +4,10 @@ rm index.md
 touch index.md
 cat intro.md >> index.md
 cat education.md >> index.md
+cat research_projects.md >> index.md
 cat publications.md >> index.md
 cat oss.md >> index.md
 cat patent.md >> index.md
-cat research_projects.md >> index.md
 cat talks.md >> index.md
 cat strength.md >> index.md
 cat lecture.md >> index.md
